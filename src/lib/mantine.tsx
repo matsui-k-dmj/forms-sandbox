92da4432e9429e9bfb8e1d25c3ad7bd5cefbfc32
matsui-k-dmj <osskil611@gmail.com>
2023-08-07 01:28:05 +0900

    Postする。Postする前にvalidationする

--- src/lib/mantine.tsx
+++ src/lib/mantine.tsx
@@ -2,11 +2,12 @@ import {
   TextInput as _TextInput,
   Textarea as _Textarea,
   Select as _Select,
+  Button as _Button,
 } from '@mantine/core';
 
 import { DateInput as _DateInput } from '@mantine/dates';
 
-import { ComponentProps, memo } from 'react';
+import { ComponentProps, ReactNode, memo } from 'react';
 
 export const TextInput = memo(function TextInput(
   props: ComponentProps<typeof _TextInput>
@@ -34,4 +35,13 @@ export const DateInput = memo(function DateInput(
 ) {
   console.log(`Rendering DateInput ${props.label}`);
   return <_DateInput {...props} />;
+});
+
+export const Button = memo(function Button(
+  props: ComponentProps<typeof _Button<'button'>> & {
+    children?: ReactNode;
+  }
+) {
+  console.log(`Rendering Button`);
+  return <_Button {...props}> {props.children} </_Button>;
 });
--- src/pages/form1.tsx
+++ src/pages/form1.tsx
@@ -1,6 +1,7 @@
 import { usersToSelectData } from '@/common/mintine-select';
-import { Select, TextInput, Textarea, DateInput } from '@/lib/mantine';
+import { Select, TextInput, Textarea, DateInput, Button } from '@/lib/mantine';
 import { ChangeEvent, useCallback, useMemo, useState } from 'react';
+import dayjs from 'dayjs';
 
 const allUsers: User[] = [
   { id: 1, name: '松山' },
@@ -20,9 +21,11 @@ type FormData = {
   endDate?: Date | null;
 };
 
+type FieldErrors = Record<keyof FormData, string[]>;
+
 export default function Form1() {
   const [formData, setFormData] = useState<FormData>({ title: '' });
-  const [errors, setErrors] = useState<Record<keyof FormData, string[]>>({
+  const [errors, setErrors] = useState<FieldErrors>({
     title: [],
     description: [],
     userIdAssingnedTo: [],
@@ -40,14 +43,7 @@ export default function Form1() {
         return { ...prev, title: value };
       });
 
-      const newErros: string[] = [];
-      if (value === '') {
-        newErros.push('必須');
-      }
-      if (value.length >= 21) {
-        newErros.push('20文字以内');
-      }
-      setErrors((prev) => ({ ...prev, title: newErros }));
+      setErrors((prev) => ({ ...prev, title: validateTitle(value) }));
     },
     [setFormData]
   );
@@ -70,16 +66,12 @@ export default function Form1() {
         userIdAssingnedTo: value,
       }));
 
-      const newErros: string[] = [];
-      if (value != null && formData.userIdVerifiedBy != null) {
-        if (value === formData.userIdVerifiedBy) {
-          newErros.push('担当者と承認者が同じです');
-        }
-      }
+      const newErrors = validateUsers(value, formData.userIdVerifiedBy);
+
       setErrors((prev) => ({
         ...prev,
-        userIdAssingnedTo: newErros,
-        userIdVerifiedBy: newErros,
+        userIdAssingnedTo: newErrors,
+        userIdVerifiedBy: newErrors,
       }));
     },
     [formData.userIdVerifiedBy]
@@ -92,17 +84,12 @@ export default function Form1() {
         userIdVerifiedBy: value,
       }));
 
-      const newErros: string[] = [];
-      if (value != null && formData.userIdAssingnedTo != null) {
-        if (value === formData.userIdAssingnedTo) {
-          newErros.push('担当者と承認者が同じです');
-        }
-      }
+      const newErrors = validateUsers(formData.userIdAssingnedTo, value);
 
       setErrors((prev) => ({
         ...prev,
-        userIdAssingnedTo: newErros,
-        userIdVerifiedBy: newErros,
+        userIdAssingnedTo: newErrors,
+        userIdVerifiedBy: newErrors,
       }));
     },
     [formData.userIdAssingnedTo]
@@ -115,16 +102,11 @@ export default function Form1() {
         startDate: value,
       }));
 
-      const newErros: string[] = [];
-      if (value != null && formData.endDate != null) {
-        if (formData.endDate < value) {
-          newErros.push('開始日が終了日よりも後になっています');
-        }
-      }
+      const newErrors = validateDate(value, formData.endDate);
       setErrors((prev) => ({
         ...prev,
-        startDate: newErros,
-        endDate: newErros,
+        startDate: newErrors,
+        endDate: newErrors,
       }));
     },
     [formData.endDate]
@@ -137,20 +119,62 @@ export default function Form1() {
         endDate: value,
       }));
 
-      const newErros: string[] = [];
-      if (value != null && formData.startDate != null) {
-        if (value < formData.startDate) {
-          newErros.push('開始日が終了日よりも後になっています');
-        }
-      }
+      const newErrors = validateDate(formData.startDate, value);
       setErrors((prev) => ({
         ...prev,
-        startDate: newErros,
-        endDate: newErros,
+        startDate: newErrors,
+        endDate: newErrors,
       }));
     },
     [formData.startDate]
   );
+
+  const onPost = useCallback(() => {
+    const usersErrors = validateUsers(
+      formData.userIdAssingnedTo,
+      formData.userIdVerifiedBy
+    );
+    const dateErrors = validateDate(formData.startDate, formData.endDate);
+    const newErrors: FieldErrors = {
+      title: validateTitle(formData.title),
+      description: [],
+      userIdAssingnedTo: usersErrors,
+      userIdVerifiedBy: usersErrors,
+      startDate: dateErrors,
+      endDate: dateErrors,
+    };
+
+    setErrors(newErrors);
+    if (
+      Object.values(newErrors)
+        .map((errors) => errors.length > 0)
+        .some((hasError) => hasError)
+    ) {
+      return;
+    }
+
+    const payload: TaskPostPayload = {
+      title: formData.title,
+      description: formData.description,
+      user_id_assingned_to:
+        formData.userIdAssingnedTo == null
+          ? undefined
+          : Number(formData.userIdAssingnedTo),
+      user_id_verified_by:
+        formData.userIdVerifiedBy == null
+          ? undefined
+          : Number(formData.userIdVerifiedBy),
+      start_date:
+        formData.startDate == null
+          ? undefined
+          : dayjs(formData.startDate).format('YYYY-MM-DD'),
+      end_date:
+        formData.endDate == null
+          ? undefined
+          : dayjs(formData.endDate).format('YYYY-MM-DD'),
+    };
+    console.log('Submit', payload);
+  }, [formData]);
 
   return (
     <div className="p-20">
@@ -202,6 +226,7 @@ export default function Form1() {
           valueFormat="YYYY/MM/DD"
           clearable
           value={formData.startDate}
+          maxDate={formData.endDate ?? undefined}
           onChange={onChangeStartDate}
           error={errors.startDate.join(', ')}
         />
@@ -212,10 +237,48 @@ export default function Form1() {
           valueFormat="YYYY/MM/DD"
           clearable
           value={formData.endDate}
+          minDate={formData.startDate ?? undefined}
           onChange={onChangeEndDate}
           error={errors.endDate.join(', ')}
         />
       </div>
+      <div>
+        <Button onClick={onPost}>Submit</Button>
+      </div>
     </div>
   );
+}
+
+function validateTitle(value: string) {
+  const newErrors: string[] = [];
+  if (value === '') {
+    newErrors.push('必須');
+  }
+  if (value.length >= 21) {
+    newErrors.push('20文字以内');
+  }
+  return newErrors;
+}
+
+function validateUsers(
+  userIdAssingnedTo?: string | null,
+  userIdVerifiedBy?: string | null
+) {
+  const newErrors: string[] = [];
+  if (userIdAssingnedTo != null && userIdVerifiedBy != null) {
+    if (userIdAssingnedTo === userIdVerifiedBy) {
+      newErrors.push('担当者と承認者が同じです');
+    }
+  }
+  return newErrors;
+}
+
+function validateDate(startDate?: Date | null, endDate?: Date | null) {
+  const newErrors: string[] = [];
+  if (startDate != null && endDate != null) {
+    if (endDate < startDate) {
+      newErrors.push('開始日が終了日よりも後になっています');
+    }
+  }
+  return newErrors;
 }
