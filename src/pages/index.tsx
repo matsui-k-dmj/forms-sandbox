31f109bf27911acce4ade59c47401e6ccd304a5b
matsui-k-dmj <osskil611@gmail.com>
2023-08-15 22:54:02 +0900

    add doc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,18 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="p-20">
-      <Link href="form1">form1</Link>
+      <div className="my-2">
+        <Link href="form1">form1</Link>
+      </div>
+      <div className="my-2">
+        <Link href="form2">form2</Link>
+      </div>
+      <div className="my-2">
+        <Link href="form2-dynamic">form2-dynamic</Link>
+      </div>
+      <div className="my-2">
+        <Link href="list-form">list-form</Link>
+      </div>
     </div>
   );
 }
