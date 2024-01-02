4faca19a28857478ca45e681a215af51526c4c71
matsui-k <osskil611@gmail.com>
2024-01-02 22:19:32 +0900

    type utils

--- src/common/filter-falsy.ts
+++ src/common/filter-falsy.ts
@@ -1,3 +1,5 @@
-export function filterFalsy<T>(v: T): v is NonFalsy<T> {
+import { Falsy } from './type-utils';
+
+export function filterFalsy<T>(v: T): v is Exclude<T, Falsy> {
   return Boolean(v);
 }
--- src/common/type-utils.ts
+++ src/common/type-utils.ts
@@ -1,3 +1,1 @@
 export type Falsy = false | 0 | '' | null | undefined;
-
-export type NonFalsy<T> = Exclude<T, Falsy>;
